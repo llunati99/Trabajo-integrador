124f329e0312e785908ea14a7fe20dcc209234d0
Valentin <valentinsanchezm@hotmail.com>
2018-05-12 17:57:43 -0300

    agregue una estructura basica al index y cree la hoja de estilos

--- index.php
+++ index.php
@@ -1,9 +1,62 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-	<p>hola</p>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<!-- Latest compiled and minified CSS(BOOTSRAP) -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Nuestra Empresa</title>
+	</head>
+
+
+	<body>
+		<div class="container">
+
+			<!-- Menu -->
+			<div class="row">
+				<div class="col-xs-12">
+				</div>
+			</div>
+
+			<!-- Imagenes/Video promocion -->
+			<div class="row">
+				<div>
+				</div>
+			</div>
+
+			<!-- Motos/Productos a la venta -->
+			<div class="row">
+				<div>
+				</div>
+			</div>
+
+			<!-- Footer -->
+			<div class="row">
+			</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		</div>
+
+	</body>
 </html>
