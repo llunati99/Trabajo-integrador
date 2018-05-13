64811c6500c4ec015854a0ca2ec8382a7688185b
Valentin <valentinsanchezm@hotmail.com>
2018-05-13 20:41:04 -0300

    arregle la estructura general del index en mobile friendly (ver en 767px) y agregue imagenes y valores al css

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		<meta charset="utf-8">
 		<!-- Latest compiled and minified CSS(BOOTSRAP) -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Nuestra Empresa</title>
 	</head>
@@ -18,19 +19,35 @@
 
 			<!-- Menu -->
 			<div class="row">
-				<div class="col-xs-12">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 menu">
+					<img class="logomoto" src="images/logomoto.png" alt="logo moto">
+					<img class="icono-menu" src="images/menu-icon.png" alt="icono menu">
 				</div>
 			</div>
 
 			<!-- Imagenes/Video promocion -->
 			<div class="row">
-				<div>
+				<div class="col-xs-12 box-promocion">
+					<img class="promocion-moto" src="images/promocion-moto.jpg" alt="banner promocion motos">
 				</div>
 			</div>
 
 			<!-- Motos/Productos a la venta -->
 			<div class="row">
-				<div>
+				<div class="col-xs-12 box-productos-grande">
+					<div class="box-producto">
+						<img class="moto-img" src="images/moto1.jpg" alt="foto motocicleta">
+						<br>
+						<div class="box-texto">
+							<h2 class="titulo-producto"> Harley Davidson SUPERLOW </h2>
+						</div>
+						<div class="box-producto">
+							<img class="moto-img" src="images/moto2.jpg" alt="foto motocicleta">
+							<br>
+							<div class="box-texto">
+								<h2 class="titulo-producto"> Harley Davidson ULTRA LIMITED</h2>
+							</div>
+					<div>
 				</div>
 			</div>
 
