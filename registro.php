d68cd2cefdacf6b8d35f580237069b251c407a82
Luca Lunati <llunati99@gmail.com>
2018-05-15 09:40:59 -0300

    Adjunte los css

--- registro.php
+++ registro.php
@@ -22,7 +22,7 @@ if ($_POST) {
 	<meta charset="utf-8">
 	<title>Registro</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body class="container">
 	<div class="header">
